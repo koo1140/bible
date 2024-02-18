f61e328c4e576cdf35f3fca7839b37900c521cdd
Mmm <112756430+koo1140@users.noreply.github.com>
2024-02-18 08:26:47 +0000

    rrrrr

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import search
 import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='.', help_command=None, intents=intents)
+
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="hi"))
+    print(f'We have logged in as {bot.user}')
+
 
 def query(l, n, c, v, e):
     file_path = os.path.join("json", f"{l}_{n}.json")
@@ -7,4 +18,16 @@ def query(l, n, c, v, e):
     result = search.search_verses_in_json(verses, c, v, e)
     return result
 
-print(query("en", "kjv", "1", "1", "1"))
+#print(query("en", "kjv", "1", "1", "1"))
+
+@bot.command(name="verse", help="Gives a bible verse.")
+async def verse(ctx, *, text="nothing"):
+  # Create an embed
+  global message_count
+  message_count += 1
+  async with ctx.typing():
+    result = ""
+  # Send the embed as a message
+  await ctx.send(result)
+
+bot.run("MTAxNjcwMzMwMDU5MjIyNjM4NA.Gv1ZT7.ZDG4zH69vNMD_opm_xBjOGCDg3ldtzYbvG_rHU")
